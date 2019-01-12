1068a48b6e8158806c7598050a52ba6a4abf05ea
bencsbalazs@gmail.com <bencsbalazs@gmail.com>
2019-01-12 23:06:45 +0100

    Dynamic content load

---
+++ scripts/functions.js
@@ -0,0 +1,23 @@
+const list = ["","","",""]
+let urls = []
+$(() => {
+    $.get("https://picsum.photos/list").then((images) => {
+        $.map(list, () => {
+            urls.push(`https://picsum.photos/900/500?image=${images[Math.floor(Math.random() * images.length)].id}`)
+        })
+    }).then(() => {
+        urls.forEach((index, element)=>{
+            active=(element == 0) ? " active": "" 
+            $("<div class='carousel-item"+active+"'><img src='"+index+"'></div>").appendTo(".carousel-inner").eq(0)
+        })
+    })
+});
+
+sloganToggle = () => {
+    const currentScrollPos = window.pageYOffset;
+      if (this.prevScrollPos >= currentScrollPos) {
+        $('.carousel-caption').css('display', 'block');
+      } else {
+        $('.carousel-caption').css('display', 'none');
+      }
+  }
