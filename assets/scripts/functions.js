84c8b7788aa0d70a7dd6742137b1ecb7ed40846c
Balázs Bencs <balazs.bencs@ibm.com>
2019-05-03 16:52:05 +0200

    about me

--- assets/scripts/functions.js
+++ assets/scripts/functions.js
@@ -1,29 +1,20 @@
 let urls = []
+let modalId = $('#image-gallery');
 $(() => {
     $.get("https://picsum.photos/list").then((images) => {
-        $.map(texts.slogans, () => {
+        $.map([1,2,3], () => {
             urls.push(`https://picsum.photos/900/500?image=${images[Math.floor(Math.random() * images.length)].id}`)
         })
     }).then(() => {
-        urls.forEach((index, element)=>{
-            $("<div class='carousel-item"+((element == 0) ? ' active' : '')+"'><img src='"+index+"'><div class='carousel-caption' style='display:"+(((window.innerHeight * 0.15) >= window.pageYOffset)?'block':'none')+"'><h3>"+texts.slogans[element].slogan+"</h3><p class='pull-right'>"+texts.slogans[element].author+"</p></div></div>").appendTo(".bgslider > .carousel-inner").eq(0)
+      $("#blog").children("img").each((i)=>{
+            console.log($(this).attr(src))
         })
     })
-    $("#listByDate ul li ul").each((i,e)=>{$(e).hide()});
-    $('[data-toggle="popover"]').popover();
     wow = new WOW()
     wow.init()
-    $(document).on("click", (e) => {$(e.target).closest('li').children('ul').eq(0).toggle();});
-    $(document).on('scroll', () => {$('.bgslider .carousel-caption').css('display', ((window.innerHeight * 0.15) >= window.pageYOffset)?'block':'none');});
     $(window).on('activate.bs.scrollspy', (e) => {
         history.replaceState({}, "", $('.nav-item .active').attr("href"));
     });
-});
-let modalId = $('#image-gallery');
-
-$(document)
-  .ready(function () {
-
     loadGallery(true, 'a.thumbnail');
 
     //This function disables buttons when needed
@@ -86,7 +77,7 @@ $(document)
           updateGallery($(this));
         });
     }
-  });
+});
 
 // build key actions
 $(document)
